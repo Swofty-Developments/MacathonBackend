62d11dbf7e950a0c7bc036a5975a9c91aca6941e
Ian <iankk9743@gmail.com>
2025-04-24 21:56:45 +1000

    Basic db (jason don't kill me)

--- src/modules/db/__init__.py
+++ src/modules/db/__init__.py
@@ -1,6 +1,9 @@
 import os
 import logging
 
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.server_api import ServerApi
+
 # TODO: Implement a MongoDB connection here.
 
 from .collections import CollectionRef
@@ -10,7 +13,19 @@ __all__ = ["get_db", "UserRef", "CollectionRef"]
 
 _log = logging.getLogger("uvicorn")
 
+class MongoClient:
+    def __init__(self, uri):
+        self.db = AsyncIOMotorClient(uri).database
+
+    async def insert(self, collection, document):
+        return await self.db[collection].insert_one(document)
+    
+    async def find(self, collection, query):
+        return await self.db[collection].find_one(query)
+
+
 
 # TODO: Add type hint for the database connection return type.
-def get_db(endpoint: str, token: str = None):
-    token = token if token is not None else os.getenv("MONGODB_URI")
+def get_db(uri: str = None):
+    uri = uri if uri is not None else os.getenv("MONGODB_URI")
+    return MongoClient(uri)
